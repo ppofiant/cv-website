fcbb4cbeb757f683ad723118065811b5ebb64918
ppofiant <ppofiant26@gmail.com>
2020-09-28 19:57:08 +0800

    update contact form

--- js/script.js
+++ js/script.js
@@ -3,65 +3,62 @@ var validEmail = "true";
 var validPhone = "true";
 
 function validateName() {
-    var firstName = document.getElementById("first-name");
-    var lastName = document.getElementById("last-name");
-    var firstText = firstName.value;
-    var lastText = lastName.value;
+  var firstName = document.getElementById("first-name");
+  var lastName = document.getElementById("last-name");
+  var firstText = firstName.value;
+  var lastText = lastName.value;
 
-    if (firstText.length <= 0) {
-        firstName.style.border = "2px solid red";
-        validName = "false";
-    }
-    else if (lastText <= 0) {
-        lastName.style.border = "2px solid red";
-        validName = "false"
-    }
-    else {
-        lastName.style.border = "none";
-        firstName.style.border = "none";
-        validName = "true";
-    }
+  if (firstText.length <= 0) {
+    validName = "false";
+  } else if (lastText <= 0) {
+    validName = "false";
+  } else {
+    validName = "true";
+  }
 }
 
 function validateEmail() {
-    var email = document.getElementById("email");
-    var text = email.value;
-    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  var email = document.getElementById("email");
+  var text = email.value;
+  var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-    if (text.match(pattern)) {
-        email.style.border = "none";
-        validEmail = "true";
-    }
-    else {
-        validEmail = "false";
-        email.style.border = "2px solid red";
-    }
+  if (text.match(pattern)) {
+    email.style.border = "none";
+    validEmail = "true";
+  } else {
+    validEmail = "false";
+  }
 }
 
 function validatePhoneNum() {
-    var phone = document.getElementById("phone-num");
-    var text = phone.value;
-    var pat = '+62';
+  var phone = document.getElementById("phone-num");
+  var text = phone.value;
+  var pat = "+62";
 
-    if (!(pat == text.substring(0, 3))) {
-        validPhone = "false";
-        phone.style.border = "2px solid red";
-    }
-    else {
-        validPhone = "true";
-        phone.style.border = "none";
-    }
+  if (!(pat == text.substring(0, 3))) {
+    validPhone = "false";
+  } else {
+    validPhone = "true";
+  }
 }
 
 function validateAll() {
-    validateName();
-    validateEmail();
-    validatePhoneNum();
+  validateName();
+  validateEmail();
+  validatePhoneNum();
 
-    if (validName == "true" && validPhone == "true" && validEmail == "true") {
-        return alert("Success");
-    }
-    else {
-        return alert("Not Success. Please try again");
-    }
+  if (validName == "true" && validPhone == "true" && validEmail == "true") {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+setInterval(() => {
+  var btn = document.getElementById("button_send");
+  if (validateAll() == true) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+}, 500);
